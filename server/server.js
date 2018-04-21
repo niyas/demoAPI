9d93e11d021a4468d0ddfcd244319af50282d22a
Mohammed Niyas <Mohammedniyas@MSSPLLAP066.meritgroup.com>
2018-04-21 08:28:42 +0100

    additional comment

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.start = function() {
   });
 };
 
-// Bootstrap the application, configure models, datasources and middleware.
+// Bootstrap the application, configure models, datasources and middleware...
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
   if (err) throw err;
