a89763bfd9fdc8340fb83aad2c4f643dfb871950
Mohammed Niyas <Mohammedniyas@MSSPLLAP066.meritgroup.com>
2018-04-21 08:35:48 +0100

    adding commit

--- server/models/deligate.js
+++ server/models/deligate.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//test
 module.exports = function(Deligate) {
 
 };
--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 'use strict';
 
-// External Modules
+// External Modules....
 var loopback = require('loopback');
 var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
 app.start = function() {
-  // start the web server
   return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
@@ -21,6 +20,7 @@ app.start = function() {
 
 // Bootstrap the application, configure models, datasources and middleware...
 // Sub-apps like REST API are mounted via boot scripts.
+// test commit
 boot(app, __dirname, function(err) {
   if (err) throw err;
 
